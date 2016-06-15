42bf0489a328f75a766870b8269bc847190cac0c
valentinebm <valentine.barbierm@gmail.com>
2016-06-15 09:44:29 +0200

    Lost car.rb file replaced by Lorenzo's

---
+++ Week_1/Day_2/car.rb
@@ -0,0 +1,83 @@
+#require "pry"
+
+class Car
+ def initialize(sound)
+  @sound =sound
+  @visited_cities = []
+ end
+
+  def make_sound
+    puts @sound
+  end
+
+  def self.model # with self you dont have to have an actual car to call the method
+    puts "Masseratti"
+  end
+
+  def speed_control
+    speed = gets.chomp.to_i
+    if speed > 100 && speed <=120
+      puts "too fast man, calm down"
+    elsif speed > 120
+      puts " way too fast, a ticket is coming..."
+
+    end
+
+  end
+
+
+  def add_cities
+
+    puts "have you visited new cities?"
+    new_cities = gets.chomp
+
+    while new_cities == "yes" do
+        puts "which city?"
+        city = gets.chomp
+        @visited_cities.push (city)
+        puts "have you visited new cities?"
+        new_cities = gets.chomp
+    end
+  end
+
+    # def cities (city)
+    #   @visited_cities.push(city)
+    # #  binding.pry
+    # end
+    # def print_cities
+    #     IO.write ("doccities.txt", @visited_cities)
+    #     IO.read ("doccities.txt")
+    # end
+    def visited (save = true)
+        if save
+          IO.write("#{self.object_id}.txt", @visited_cities)
+        else
+          puts "here are the cities i visited"
+          @visited_cities.each do |item|
+            puts "---> #{item}"
+          end
+        end
+    end
+end
+
+class RacingCar < Car
+  def make_sound # overwrites on top of the class. no matter what you put as sound it will always puts BROOOOOOOMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    puts "BROOOOOOOMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  end
+end
+
+mini = Car.new ("BROOOOOOM")
+jaguar = Car.new ("brooom")
+
+mini.make_sound
+jaguar.make_sound
+
+jaguar.speed_control
+jaguar.add_cities
+# jaguar.cities("Barcelona")
+# jaguar.cities("Madrid")
+
+racing_car = RacingCar.new ("bbbbb")
+racing_car.make_sound
+#jaguar.print_cities
+jaguar.visited
