caf0bb3814f1112f879e9a8832f0b9f185b9b568
valentinebm <valentine.barbierm@gmail.com>
2016-06-17 17:19:38 +0200

    class added for queen, function added for diagonal movement. programm works for both rook and queen now. YAY

--- Week_1/Day_5/chess_validator.rb
+++ Week_1/Day_5/chess_validator.rb
@@ -157,6 +157,105 @@ end
 
 end
 
+class Queen
+
+  def initialize
+  end
+
+  def moves(origin, destination)
+    @origin = origin
+    @destination = destination
+
+    if @origin.horizontal == @destination.horizontal || @origin.vertical == @destination.vertical || diagonal(@origin, @destination)
+    return true
+    end
+  end
+
+  def diagonal(origin, destination)
+    @origin = origin
+    @destination = destination
+
+    diff1 =  @origin.horizontal - @destination.horizontal
+    diff1 =  @origin.vertical - @destination.vertical
+
+    if diff1.abs == diff1.abs
+      return true
+    end
+  end
+
+  def check_which_move(origin, destination)
+    @origin = origin
+    @destination = destination
+
+    if @origin.horizontal == @destination.horizontal
+      return "horizontal"
+
+    elsif @origin.vertical == @destination.vertical
+      return "vertical"
+    end
+  end
+
+  def all_empty(array)
+    @array = array
+    results = []
+
+    @array.each do |x,y|
+      position = Position.new(x,y)
+      result = position.show(@board)
+      results.push(result)
+      end
+
+    if results.include? true
+      answer = false
+    else
+      answer = true
+    end
+    answer
+  end
+
+def all_cell_betweens(origin, destination)
+  @origin = origin
+  @destination = destination
+  @array = []
+
+  if check_which_move(@origin,@destination).eql? "horizontal"
+    for i in @origin.vertical+1...@destination.vertical
+      cell = [@origin.horizontal, i]
+      @array.push(cell)
+    end
+
+  elsif check_which_move(@origin,@destination).eql? "vertical"
+      for i in @origin.horizontal+1...@destination.horizontal
+        cell = [i, @origin.vertical]
+        @array.push(cell)
+      end
+    end
+    @array
+end
+
+  def check_move_validity(board, origin, destination)
+    @board = board
+    @origin = origin
+    @destination = destination
+
+    if moves(@origin, @destination) == true
+      puts "Move is possible"
+
+    if all_empty(all_cell_betweens(@origin, @destination)) == true
+      puts "Move is VALID!!!!"
+
+    else
+      puts "Move isn't valid - something in the way"
+    end
+
+    else
+      puts "Pawn not able to perform that move"
+    end
+    #verify whether cells until destination are free
+  end
+
+end
+
 grid =
 
   [ :bR, nil, nil, nil, nil, nil, nil, nil ],
@@ -172,6 +271,8 @@ my_board = Board.new(grid)
 
 first_position = Position.new(0,0)
 last_position = Position.new(0,7)
+d8 = Position.new(0,4)
+a5 = Position.new(3,0)
 # other_position = Position.new(0,1)
 
 # my_board.show_occupation(first_position)
@@ -179,4 +280,6 @@ last_position = Position.new(0,7)
 # starting_point.show_position
 
 bR = Rook.new
-bR.check_move_validity(grid, first_position, last_position)
+bQ = Queen.new
+# bR.check_move_validity(grid, first_position, last_position)
+bQ.check_move_validity(grid, d8, a5)
