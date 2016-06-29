f3ef156131d4c7b78549bdc97b80a3acb6359ef7
valentinebm <valentine.barbierm@gmail.com>
2016-06-29 17:46:13 +0200

    cipher exercise iteration 4

--- Week_3/Day_3/caesar_cipher/cipher.js
+++ Week_3/Day_3/caesar_cipher/cipher.js
@@ -1,18 +1,34 @@
 function caesarCipher(message, shift=3) {
   var letters = message.split('');
   var ascii_switched = letters.map(function(letter) {
-    if ((letter.charCodeAt()>90 && letter.charCodeAt()<97) || (letter.charCodeAt()>122 || letter.charCodeAt()<65)) {
-    return letter.charCodeAt();
+    var number = letter.charCodeAt();
+    if ((number>90 && number<97) || (number>122) || (number<65)) {
+    return letter;
     }
+
     else {
-      return letter.charCodeAt()+shift;
+
+      if (number + shift > 122) {
+        letter = String.fromCharCode(number+shift-26)
+      }
+
+      else if (number + shift < 65) {
+        letter = String.fromCharCode(number+shift+26)
+      }
+
+      else {
+        letter = String.fromCharCode(number+shift)
+      }
+
+      return letter
     }
+
   })
-  var new_message = ascii_switched.map(function(number){
-    return String.fromCharCode(number)
-  }).join("")
-  return new_message;
+  var new_message = ascii_switched.join("")
+  return new_message
 }
+
+
 
 var encrypted = caesarCipher("Et tu, brute?", 6);
 
