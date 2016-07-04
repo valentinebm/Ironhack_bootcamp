2ee10710e1f337f052c3ca7d7f2aa9102dd1a8e5
valentinebm <valentine.barbierm@gmail.com>
2016-07-04 10:32:50 +0200

    two routes added

--- Week_4/Day_1/timetracking/config/routes.rb
+++ Week_4/Day_1/timetracking/config/routes.rb
@@ -1,3 +1,11 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'site#home'
+end
+
+Rails.application.routes.draw do
+  get '/contact', to: 'site#contact'
 end
