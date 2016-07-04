d4b27a4076c489cc909c903ccba6f8b28d8ee1df
valentinebm <valentine.barbierm@gmail.com>
2016-07-04 12:55:59 +0200

    repo cloned

--- Week_4/Day_1/timetracking/config/routes.rb
+++ Week_4/Day_1/timetracking/config/routes.rb
@@ -4,8 +4,8 @@
 
 Rails.application.routes.draw do
   get '/', to: 'site#home'
-end
-
-Rails.application.routes.draw do
   get '/contact', to: 'site#contact'
+  get '/say_name/:name', to: 'site#say_name'
+  get '/calculator', to: 'site#calculator'
+  post '/calculate', to: 'site#calculate'
 end
