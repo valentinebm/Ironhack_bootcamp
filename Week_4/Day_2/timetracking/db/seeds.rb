5ac22ebeb30d6afd556889eded277fa52d7c4fc9
valentinebm <valentine.barbierm@gmail.com>
2016-07-05 12:09:44 +0200

    standard database created with seeds

--- Week_4/Day_2/timetracking/db/seeds.rb
+++ Week_4/Day_2/timetracking/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.create(name: "Crazy project", description: "You won't believe it")
+Project.create(name: "Another project", description: "So much to say")
+Project.create(name: "Life in Barcelona")
