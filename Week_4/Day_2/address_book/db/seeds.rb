b6ea7b0349dc0a5396bff6bf53845b54b3d863b9
valentinebm <valentine.barbierm@gmail.com>
2016-07-05 15:49:32 +0200

    address_book app created

---
+++ Week_4/Day_2/address_book/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create(name: "Marie", address:"2 avenue Godefroy, 1208, Geneva, Switzerland", phone_number: 0041797479257, email_address: "mbarbierm@gmail.com")
+Contact.create(name: "Zoé", address:"2 avenue Godefroy, 1208, Geneva, Switzerland", phone_number: 0041793323962, email_address: "zbarbierm@gmail.com")
+Contact.create(name: "Titi", address:"2 avenue Godefroy, 1208, Geneva, Switzerland", phone_number: 0041795987021, email_address: "tbm@spg.com")
+Contact.create(name: "Béné", address:"Rue du Grand-Lancy", phone_number: 004176269222, email_address: "b.montant@3bm3.com")
