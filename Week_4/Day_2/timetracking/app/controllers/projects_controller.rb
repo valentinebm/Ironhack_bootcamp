f458cb53d572ba338ac4d383c66148154427f56a
valentinebm <valentine.barbierm@gmail.com>
2016-07-06 10:29:44 +0200

    updated timetracking

--- Week_4/Day_2/timetracking/app/controllers/projects_controller.rb
+++ Week_4/Day_2/timetracking/app/controllers/projects_controller.rb
@@ -10,4 +10,17 @@ class ProjectsController < ApplicationController
     end
     # when you use find, it's more difficult to handle the exception
   end
+
+  def new
+    @project = Project.new
+    render 'new'
+  end
+
+  def create
+    @project = Project.new(name: params[:project][:name], description: params[:project][:description])
+
+    @project.save
+
+    redirect_to '/projects/#{@project.id}'
+  end
 end
--- Week_4/Day_2/timetracking/config/routes.rb
+++ Week_4/Day_2/timetracking/config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   post '/calculate', to: 'site#calculate'
 
   get '/projects', to: 'projects#index'
+  get '/projects/new', to: 'projects#new'
   get '/projects/:id', to: 'projects#show'
+
+  post '/projects', to: 'projects#create'
 end
