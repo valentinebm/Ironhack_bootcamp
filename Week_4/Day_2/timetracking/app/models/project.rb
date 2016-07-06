e2cf306bbdc91bcf841537ada677b499c8b7c219
valentinebm <valentine.barbierm@gmail.com>
2016-07-06 13:14:42 +0200

    verifications added

--- Week_4/Day_2/timetracking/app/models/project.rb
+++ Week_4/Day_2/timetracking/app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   has_many :time_entries
+
+  validates :name, presence: true, uniqueness: true, length: {maximum: 30}, format: {with: /\[a-zA-Z\s]/}
 end
--- Week_4/Day_2/timetracking/app/models/time_entry.rb
+++ Week_4/Day_2/timetracking/app/models/time_entry.rb
@@ -1,3 +1,8 @@
 class TimeEntry < ApplicationRecord
   belongs_to :project
+
+  validates :datetime, presence: true
+  validates :hours, presence: true, numericality: true
+
+  validates :minutes, presence: true, numericality: true
 end
