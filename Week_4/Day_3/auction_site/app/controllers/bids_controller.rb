aa942f977c3c2588bbb4c81cd3266d0ea98aa0a4
valentinebm <valentine.barbierm@gmail.com>
2016-07-08 10:34:07 +0200

    bids form working

--- Week_4/Day_3/auction_site/app/controllers/bids_controller.rb
+++ Week_4/Day_3/auction_site/app/controllers/bids_controller.rb
@@ -1,8 +1,8 @@
 class BidsController < ApplicationController
   def create
     @product = Product.find_by(id: params[:id])
-    @email = params[:bid][:user_email]
-    @user = User.find_by(email: params[@email])
+    @email = params[:bid_email]
+    @user = User.find_by(email: @email)
     @bid = Bid.new(user_id: @user.id, amount: params[:bid][:amount], product_id: @product.id)
     @bid.save
     redirect_to '/products'
--- Week_4/Day_3/auction_site/db/migrate/20160706144305_add_user_reference.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReference < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :products, :user, index: true
-  end
-end
