3eca58d84969ec6cebdf0ee56ce3e7c7f49b9f26
valentinebm <valentine.barbierm@gmail.com>
2016-07-11 11:33:47 +0200

    Week 5 Day 1 morning exercise cell changing color

---
+++ Week_5/Day_1/morning-2/index.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+  setInterval(function(){
+    var cell = $('.cell');
+    cell.toggleClass('blue red');
+  }, 400);
+
+
+// var selectorvalue = $('')
+//
+// console.log(selectorvalue);
+
+
+$('p').empty();
