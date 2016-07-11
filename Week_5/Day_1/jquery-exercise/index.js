6f5da0cc83294809d4c6c73a51211a99746cee12
valentinebm <valentine.barbierm@gmail.com>
2016-07-11 19:06:49 +0200

    phrases exercise completed up to iteration 4

--- Week_5/Day_1/jquery-exercise/index.js
+++ Week_5/Day_1/jquery-exercise/index.js
@@ -2,6 +2,7 @@ var phrases = [
   "The coolest phrase.", "Let's go to the movies.", "Le compte avait commencé à rebours."
 ];
 
+var list = $('ul.allsentences')
 
 function loadPhrase(){
   var phrase = phrases[Math.floor(Math.random()*phrases.length)];
@@ -18,7 +19,37 @@ $(".addphrase").submit(function(e){
   e.preventDefault();
   var newphrase = $('#newphrase').val();
   phrases.push(newphrase);
-  console.log(phrases);
+  showNewPhrase(newphrase)
 })
+
+//to be kept and add function phrasesdisplay(){
+
+$(".viewphrase").click(function(e){
+  e.preventDefault();
+  $(".viewphrase").toggleClass('active');
+  if ($(".viewphrase").hasClass('active')) {
+    $.each(phrases, function() {
+      var li = $('<li/>')
+        .appendTo(list);
+      var content = $('<p/>')
+        .text(this)
+        .appendTo(li);
+    })
+  }
+  else {
+    ($(".allsentences").text(""));
+}
+});
+
+
+function showNewPhrase(newphrase){
+      var li = $('<li/>')
+        .appendTo(list);
+      var content = $('<p/>')
+        .text(newphrase)
+        .appendTo(li);
+}
+
+
 
 loadPhrase();
