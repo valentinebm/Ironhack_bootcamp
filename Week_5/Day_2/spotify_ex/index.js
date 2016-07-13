5f9b95d2f289220b4ff22945ead18c58a35052d7
valentinebm <valentine.barbierm@gmail.com>
2016-07-13 15:28:54 +0200

    spotify refactorized with functions

--- Week_5/Day_2/spotify_ex/index.js
+++ Week_5/Day_2/spotify_ex/index.js
@@ -1,64 +1,83 @@
 $('.searchbutton').on('click', function(e) {
   e.preventDefault();
   var keyword = $('[name="artist"]').val();
+  getData(keyword)
+});
 
+function getData(keyword){
   $.ajax({
       type:'GET',
       url: 'https://api.spotify.com/v1/search?type=artist&query='+keyword,
       success: function(response) {
-        $( "ul.artistlist" ).empty();
-        $( ".results" ).empty();
+        cleanLeftSide();
 
         var artistarray = response.artists.items;
         var index = 1;
         var results = $('<h1>').text("Results")
-        $('.results').append(results)
+        $('.results').append(results);
 
-        artistarray.forEach( function(artist) {
-          var listItem = $('<a href="">').text(artist.name)
-          $('ul.artistlist').append(listItem)
-          $('ul.artistlist').append($('<hr>'))
-          listItem.addClass("link" + index++)
-          listItem.attr('id', artist.id)
-          listItem.addClass("link")
-
-            if (artist.images != '') {
-              var link = artist.images[0].url
-              var image = $('<img>').attr('src', link);
-              listItem.append(image)
-            }
-
-        })
+        displayArtists(artistarray, index)
 
 
-        $('.link').on('click', function(e) {
+        $('a').on('click', function(e) {
           e.preventDefault();
-          $( "ul.albums" ).empty();
-          $( ".albumstitle" ).empty();
-          artistid = this.id
+          cleanRightSide();
+          artistid = this.id,
           url = 'https://api.spotify.com/v1/artists/'+artistid+'/albums'
-          var albums = $('<h3>').text("Albums")
-          $('.albumstitle').append(albums)
-
-          $.ajax({
-            type:'GET',
-            url: url,
-            success: function(response) {
-              artistalbums = response.items,
-                artistalbums.forEach( function(album){
-                  var name = $('<li>').text(album.name)
-                  $('.albums').append(name)
-                  })
-            }
-          })
+          getAlbums(artistid, url);
         })
-    },
-
-    error: function(err){
-      console.log(err)
-      console.log('in da error')
-    },
-    // datatype: "json"
+      },
   });
+}
 
-})
+function cleanLeftSide(){
+  $( "ul.artistlist" ).empty();
+  $( ".results" ).empty();
+}
+
+
+function addImage(artist, image){
+  artist.append(image)
+}
+
+function displayArtists(artists, index){
+    artists.forEach( function(artist) {
+    var listItem = $('<a href="">').text(artist.name)
+    $('ul.artistlist').append(listItem, $('<hr>'))
+    listItem.attr('id', artist.id)
+
+      if (artist.images != '') {
+        var image = $('<img>').attr('src', artist.images[0].url);
+        addImage(listItem, image)
+      }
+  })
+}
+
+function cleanRightSide(){
+  $( "ul.albums" ).empty();
+  $( ".albumstitle" ).empty();
+}
+
+function getAlbums(artistid, url){
+  addTitle()
+
+  $.ajax({
+    type:'GET',
+    url: url,
+    success: function(response) {
+      displayAlbums(response.items)
+    }
+  })
+}
+
+function addTitle(){
+  var albums = $('<h3>').text("Albums")
+  $('.albumstitle').append(albums)
+}
+
+function displayAlbums(artistalbums){
+    artistalbums.forEach( function(album){
+    var name = $('<li>').text(album.name)
+    $('.albums').append(name)
+    })
+}
