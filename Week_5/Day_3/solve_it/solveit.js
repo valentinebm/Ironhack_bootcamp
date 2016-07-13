b541470549429d151f6fc060d3acc3787ae61487
valentinebm <valentine.barbierm@gmail.com>
2016-07-13 12:37:35 +0200

    solve it corrected

--- Week_5/Day_3/solve_it/solveit.js
+++ Week_5/Day_3/solve_it/solveit.js
@@ -1,11 +1,24 @@
-$(function () {
-  for (var i = 0; i < 10; i++) {
+// $(function () {
+//   for (var i = 0; i < 10; i++) {
+//     var button = $('<button>').text('click #' + i);
+//     button.attr('id', i)
+//     $('.container').append(button);
+//     }
+//     $('button').click(function(e){
+//     e.preventDefault();
+//     console.log('You clicked on button #' + this.id)
+//   });
+// });
+
+
+//simpler
+
+$(function(){
+  for (let i=0; i<10; i++) {
     var button = $('<button>').text('click #' + i);
-    button.attr('id', i)
     $('.container').append(button);
-    }
-    $('button').click(function(e){
-    e.preventDefault();
-    console.log('You clicked on button #' + this.id)
-  });
-});
+    button.on('click', function(){
+      console.log('You clicked on the button ' + i);
+    })
+  }
+})
